59e20c8fb748a87db81cd2acc4b90d8069486fef
Даниил Цуканов <danil.czukanov@bk.ru>
2026-02-07 13:49:31 +0300

    seed and factory

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Task extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description',
+    ];
+
+
 }
